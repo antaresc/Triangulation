97e0f4d6fa2b9191c4264b24609ba104b98d610a
Antares Chen <antares21chen@gmail.com>
2016-01-02 16:42:02 -0500

    Implemented quickselect.

--- utils.py
+++ utils.py
@@ -6,8 +6,24 @@
 
 
 
-def quick_select(points, fn):
+def quick_select(points, compare):
     """Returns the median pair in points, the list of (x,y) pairs, based on the
     comparator function, fn, via the quickselect algorithm.
+
+    Compare function takes in two arguments p1, p2 and returns a positive int
+    if p1 > p2, 0 if p1 = p2, and a negative int if p1 < p2.
     """
-    pass
+    def select(points, position):
+        pivot = points[len(points) // 2]
+
+        less = [p for p in points if compare(p, pivot) < 0]
+        equal = [p for p in points if compare(p, pivot) == 0]
+        more = [p for p in points if compare(p, pivot) > 0]
+
+        if position < len(less):
+            return select(less, position)
+        elif (position > len(less) + len(equal)):
+            return select(more, position - len(less) - len(equal))
+        else:
+            return pivot
+    return select(points, len(points) // 2)
