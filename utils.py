a6df1f71b83cd7b5258d931a2d435e8358f94ae6
Antares Chen <antares21chen@gmail.com>
2016-01-02 01:10:03 -0500

    init commit

---
+++ utils.py
@@ -0,0 +1,13 @@
+"""A utility package for random algorithms.
+
+@author Antares Chen
+@since  2016-1-2
+"""
+
+
+
+def quick_select(points, fn):
+    """Returns the median pair in points, the list of (x,y) pairs, based on the
+    comparator function, fn, via the quickselect algorithm.
+    """
+    pass
